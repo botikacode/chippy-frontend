41876a523c8619da9f35b8b8a6799fbfd36db3a0
botikacode <yeraipros5@gmail.com>
2022-10-24 15:33:32 +0200

    correccion de interfaz

--- components/JobItem.js
+++ components/JobItem.js
@@ -1,37 +1,54 @@
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, Image } from 'react-native'
 import React from 'react'
 
 const JobItem = ({job}) => {
   return (
-    <View style = {styles.itemContainer}>
-      <Text style={styles.itemTitle}>{job.title}</Text>
-      <Text style={styles.itemText}>{job.description}</Text>
-      <Text style={styles.itemPrice}>{job.price}€</Text>
+    <View style = {styles.itemMainContainer}>
+      <View style = {styles.itemRightContainer}>
+        <Image source={require('../assets/accountImage.jpg')} style = {styles.itemImage}/>
+      </View>
+      <View style={styles.itemLeftContainer}>
+        <Text style={styles.itemTitle}>{job.title}</Text>
+        <Text style={styles.itemText}>{job.description}</Text>
+        <Text style={styles.itemPrice}>{job.price}€</Text>
+      </View>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
-  itemContainer: {
+  itemImage:{
+    width: 80,
+    height:80,
+    borderRadius:100,
+  },
+  itemMainContainer: {
     display: 'flex',
+    flexDirection: "row",
     backgroundColor: '#2471A3',
     marginVertical: 2,
     marginHorizontal:2,
     padding:10,
     borderRadius: 6,
   },
+  itemRightContainer:{
+    marginRight:20,
+  },
+  itemLeftContainer:{
+    flex: 1,
+  },
   itemTitle: {
     color:"#ffffff",
-    fontSize: 20,
+    fontSize: 16,
   },
   itemText: {
     color:"#ffffff",
-    fontSize: 16,
+    fontSize: 12,
   },
   itemPrice: {
     textAlign: 'right',
     color:"#ffffff",
-    fontSize: 14,
+    fontSize: 12,
   }
 });
 
