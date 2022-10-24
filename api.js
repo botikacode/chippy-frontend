be305df853104b9fabd10d40c6955b7e35f74138
botikacode <yeraipros5@gmail.com>
2022-10-24 14:55:26 +0200

    correccion de fallos menores

--- api.js
+++ api.js
@@ -1,5 +1,5 @@
-//const API = "http://localhost:3000/jobs";
-const API = "http://192.168.0.104:3000/jobs";
+const API = "http://localhost:3000/jobs";
+//const API = "http://tuip:3000/jobs";
 
 export const getJobs = async () => {
   const res = await fetch(API)
