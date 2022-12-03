7e9cf885d8fc5d2cdf1df219f2b235428069938c
Lero06 <57032316+Lero06@users.noreply.github.com>
2022-12-03 18:13:19 +0100

    Version buena para la creacion de pruebas

--- test/login.test.js
+++ test/login.test.js
@@ -1,22 +1,30 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-//import {render, screen} from '@testing-library/react'
-//import userEvent from '@testing-library/user-event'
-//import '@testing-library/jest-dom'
+import {render, screen} from '@testing-library/react-native'
+// import userEvent from '@testing-library/user-event'
+// import '@testing-library/jest-dom'
 import LoginScreen from '../screens/LoginScreen';
+import StartScreen from '../screens/StartScreen';
 
-test('renders correctly', () => {
+test('renders Login Screen correctly', () => {
   const tree = renderer.create(<LoginScreen />).toJSON();
   expect(tree).toMatchSnapshot();
 });
 
-/*test('finds text input', async () => {
+test('renders Start Screen correctly', () => {
+  const tree = renderer.create(<StartScreen />).toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+test('finds text input', async () => {
+    // ARRANGE
+    render(<StartScreen />)
   
     // ACT
-    await userEvent.click(screen.getByText('Ya tengo una cuenta'))
-    await screen.findByRole('heading')
+    // await userEvent.click(screen.getByText('Ya tengo una cuenta'))
+    //await screen.findByRole('heading')
   
     // ASSERT
-    expect(screen.getByRole('heading')).toHaveTextContent('hello there')
-    expect(screen.getByRole('button')).toBeDisabled()
-})*/
+    //expect(screen.getByRole('heading')).toHaveTextContent('hello there')
+    //expect(screen.getByRole('button')).toBeDisabled()
+})
